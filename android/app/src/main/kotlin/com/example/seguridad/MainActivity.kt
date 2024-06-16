7969968f70371394e0e89feadde2ea34e52975c0
Mauricio David Castillo Ruvalcaba <211106@ids.upchiapas.edu.mx>
2024-06-16 10:20:03 -0600

    [add] preventScreenCapture

--- android/app/src/main/kotlin/com/example/seguridad/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/seguridad/MainActivity.kt
@@ -1,6 +1,14 @@
 package com.example.seguridad
 
+
+import android.os.Bundle
+import android.view.WindowManager
 import io.flutter.embedding.android.FlutterActivity
 
-class MainActivity: FlutterActivity() {
+class MainActivity : FlutterActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        // Evitar capturas de pantalla y vistas previas
+        window.setFlags(WindowManager.LayoutParams.FLAG_SECURE, WindowManager.LayoutParams.FLAG_SECURE)
+    }
 }
